d5cde3512c1b48cd7b82809fe0d648103abf7017
Ayush Saini <ayushsaini1910@gmail.com>
2025-12-16 16:10:26 +0530

    fix bug

--- app/api/providers/github/callback/route.ts
+++ app/api/providers/github/callback/route.ts
@@ -90,7 +90,7 @@ export async function GET(req: NextRequest) {
         provider: "github",
         emailVerified: true,
         providerId: user.id,
-        lastLoginAt: Date.now,
+        lastLoginAt: Date.now(),
       });
     }
 
